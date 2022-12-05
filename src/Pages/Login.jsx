a8283bc6bf1a4b7372045e86cd238162660b3907
Novalia9517 <novalia.9517@gmail.com>
2022-12-05 17:44:06 +0700

    Create login page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -4,36 +4,37 @@ import LoginImage from '../assets/LoginImage.png'
 
 const Login = () => {
   return (
-    <div className='w-full h-screen bg-alta-primary px-16'>
-        <div className='py-7'>
-            <img src={logo} className='w-[147px]'/>
+    <div className='w-screen min-h-screen bg-alta-primary px-10 py-3 flex flex-col justify-between'>
+        <div className=''>
+            <img src={logo} className='w-36 h-18'/>
         </div>
-        <div className='flex justify-between'>
-            <div className='w-[50vw] h-full text-white font-semibold px-10'>
-                <h1 className='text-[64px]'>Welcome Back</h1>
-                <p className='text-2xl'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has</p>
-                <img src={LoginImage} alt="LoginImage" className='h-[375px] w-[571px]'/>
+        <div className='flex justify-between px-0 md:px-10 w-full'>
+            <div className='w-1/2 text-white font-semibold hidden md:block p-8'>
+                <h1 className='text-5xl'>Welcome Back</h1>
+                <p className='text-xl w-full mt-3'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has</p>
+                <img src={LoginImage} alt="LoginImage" className='w-[90%] mt-8'/>
             </div>
-            <div className='w-[50vw]'>
-                <div className="card w-[547px] bg-white text-alta-primary p-16">
-                    <h1 className='text-6xl font-semibold'>Login</h1>
-                    <p>Please enter your email and password</p>
-                    <div className="form-control w-full max-w-xs text-alta-primary">
+            <div className='grid place-items-center w-full md:w-1/2 '>
+                <div className="card w-[90%] lg:w-[70%] h-full bg-white text-alta-primary p-16">
+                    <h1 className='text-5xl font-semibold'>Login</h1>
+                    <p className='mt-3'>Please enter your email and password</p>
+                    <div className="form-control w-full text-alta- mt-10 text-xl">
                         <label className="label">
                             <span className="label-text text-alta-primary">Email</span>
                         </label>
-                        <input type="text" placeholder="" className="input border-alta-primary w-full max-w-xs bg-white caret-alta-primary" />
+                        <input type="text" placeholder="admin@gmail.com" className="input border-alta-primary w-full bg-white caret-alta-primary" />
                     </div>
-                    <div className="form-control w-full max-w-xs text-alta-primary">
+                    <div className="form-control w-full text-alta-primary mt-5 text-xl">
                         <label className="label">
                             <span className="label-text text-alta-primary">Password</span>
                         </label>
-                        <input type="text" placeholder="Type here" className="input border-alta-primary w-full max-w-xs bg-white caret-alta-primary" />
+                        <input type="text" placeholder="Password" className="input border-alta-primary w-full bg-white caret-alta-primary" />
                     </div>
-                    <button className='btn bg-alta-secondary hover:bg-hover-secondary text-white'>Login</button>
+                    <button className='btn bg-alta-secondary hover:bg-hover-secondary text-white w-full mt-10'>Login</button>
                 </div>
             </div>
         </div>
+        <p className='mt-3 text-white'>2022 © Group Project 2</p>
     </div>
   )
 }
