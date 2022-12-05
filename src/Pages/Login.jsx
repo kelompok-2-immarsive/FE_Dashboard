be31bf1eafd6eb5498db54a1fab15255ef379244
Novalia9517 <novalia.9517@gmail.com>
2022-12-05 21:05:51 +0700

    Email, password, show/hide password

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import logo from '../assets/logo-ALTA-v2@2x 1.png'
 import LoginImage from '../assets/LoginImage.png'
+import { FaRegEye } from 'react-icons/fa';
+import { FaRegEyeSlash } from 'react-icons/fa';
 import { useState } from 'react'
 
 const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [seePwd, setSeePwd] = useState(false)
   return (
     <div className='w-screen min-h-screen bg-alta-primary px-10 py-3 flex flex-col justify-between'>
         <div className=''>
@@ -21,18 +24,32 @@ const Login = () => {
                 <div className="card w-[90%] lg:w-[70%] h-full bg-white text-alta-primary p-16">
                     <h1 className='text-5xl font-semibold'>Login</h1>
                     <p className='mt-3'>Please enter your email and password</p>
-                    <form>
+                    <form className='w-full'>
                         <div className="form-control w-full text-alta- mt-10 text-xl">
                             <label className="label">
                                 <span className="label-text text-alta-primary">Email</span>
                             </label>
-                            <input type="text" placeholder="admin@gmail.com" className="input border-alta-primary w-full bg-white caret-alta-primary" onChange={(e) => setEmail(e.target.value)}/>
+                            <input type="email" placeholder="admin@gmail.com" className="input border-alta-primary w-full bg-white caret-alta-primary" onChange={(e) => setEmail(e.target.value)}/>
                         </div>
                         <div className="form-control w-full text-alta-primary mt-5 text-xl">
                             <label className="label">
                                 <span className="label-text text-alta-primary">Password</span>
                             </label>
-                            <input type="text" placeholder="Password" className="input border-alta-primary w-full bg-white caret-alta-primary" onChange={(e) => setPassword(e.target.value)}/>
+                            <div className='flex'>
+                                <div className="relative w-full">
+                                    <div 
+                                        className="absolute top-3 right-3 items-center"
+                                        onClick={() => setSeePwd(!seePwd)}>
+                                        {seePwd ? <FaRegEye /> : <FaRegEyeSlash/>}
+                                    </div>
+                                    <input
+                                        type={seePwd ? `text` : 'password'}
+                                        className="input border-alta-primary bg-white caret-alta-primary w-full caret-alta-primary"
+                                        placeholder="Password"
+                                        onChange={(e) => setPassword(e.target.value)}
+                                    />
+                                </div>
+                            </div>
                         </div>
                         <button className='btn bg-alta-secondary hover:bg-hover-secondary text-white w-full mt-10'>Login</button>
                     </form>
