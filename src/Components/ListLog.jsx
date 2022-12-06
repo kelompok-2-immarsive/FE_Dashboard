8bc470a37089a1543ac2381b053835f81da55e86
mitro-ubaidillah <ubaidillahmitro@gmail.com>
2022-12-06 19:53:12 +0700

    setup detail mentee page

--- src/Components/ListLog.jsx
+++ src/Components/ListLog.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 
-const ListLog = ({name, status, create_at, notes}) => {
+const ListLog = ({ name, status, create_at, notes }) => {
     return (
-        <div className='card'>
-            <div className="card-body flex">
-                <div>
-                    <p>{name}</p>
-                    <p>{create_at}</p>
-                </div>
-                <div>
-                    <p>{notes}</p>
-                    <p>{status}</p>
-                </div>
+        <div className='grid grid-cols-6 px-10 py-3 border-border-primary border-b'>
+            <div className='border-r-2 border-border-primary p-5'>
+                <p>{name}</p>
+                <p>{create_at}</p>
+            </div>
+            <div className='col-span-5 py-5 px-8'>
+                <p>{notes}</p>
+                <p>{status}</p>
             </div>
         </div>
     );
--- src/Pages/DetailMenteePage.jsx
+++ src/Pages/DetailMenteePage.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import ProfileMentee from '../Components/ProfileMentee';
 import ListLog from '../Components/ListLog';
 import DetailMentee from '../Components/DetailMentee';
+import { useState } from 'react';
 
 const DetailMenteePage = () => {
+    const [data, setData] = useState('profile');
+
     return (
         <div className='flex gap-6'>
             <div className='w-auto'>
@@ -19,22 +22,31 @@ const DetailMenteePage = () => {
             </div>
             <div className='card border border-border-primary w-auto'>
                 <div className="tabs">
-                    <a className="tab tab-bordered">Profile Data</a>
-                    <a className="tab tab-bordered tab-active">Feed / Log</a>
+                    <button onClick={() => setData('profile')} className={`tab tab-bordered ${data === 'profile' && `tab-active`}`}>Profile Data</button>
+                    <button onClick={() => setData('log')} className={`tab tab-bordered ${data === 'log' && `tab-active`}`}>Feed / Log</button>
                 </div>
                 <div className="card-body">
-                    <DetailMentee
-                        address={`Blitar`}
-                        category={`Informatics`}
-                        emergency_name={`Nurudin`}
-                        emergency_phone={`081772665441`}
-                        emergency_relation={`Ayah Kandung`}
-                        fullname={`Mitro`}
-                        gender={`Male`}
-                        graduate={`2022`}
-                        home_address={`Blitar`}
-                        major={`Technology Informatics`}
-                    />
+                    {data === 'profile' ?
+                        <DetailMentee
+                            address={`Blitar`}
+                            category={`Informatics`}
+                            emergency_name={`Nurudin`}
+                            emergency_phone={`081772665441`}
+                            emergency_relation={`Ayah Kandung`}
+                            fullname={`Mitro`}
+                            gender={`Male`}
+                            graduate={`2022`}
+                            home_address={`Blitar`}
+                            major={`Technology Informatics`}
+                        />
+                        :
+                        <ListLog
+                            create_at={`29 Jan 2022`}
+                            name={`Bagas`}
+                            notes={`lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolorlorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor`}
+                            status={`section change`}
+                        />
+                    }
                 </div>
             </div>
 
