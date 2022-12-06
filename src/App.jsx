a744830a5815bd7d2b4fcb878934fceffee36ee5
Aventom <ravaneloe@gmail.com>
2022-12-06 08:52:04 +0700

    nyoba resolve conflict

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Login from './Pages/Login'
-import NavbarSamping from './Components/NavbarSamping'
 import NavigationBar from './Components/NavigationBar'
 
 function App() {
