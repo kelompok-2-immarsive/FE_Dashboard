0db2ac4ff773e8544caf4e6938acdd279d7022b0
mitro-ubaidillah <ubaidillahmitro@gmail.com>
2022-12-06 19:58:46 +0700

    setup detail mentee page

--- src/Components/ListLog.jsx
+++ src/Components/ListLog.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ListLog = ({ name, status, create_at, notes }) => {
     return (
         <div className='grid grid-cols-6 px-10 py-3 border-border-primary border-b'>
-            <div className='border-r-2 border-border-primary p-5'>
+            <div className='border-r border-border-primary p-5'>
                 <p>{name}</p>
                 <p>{create_at}</p>
             </div>
