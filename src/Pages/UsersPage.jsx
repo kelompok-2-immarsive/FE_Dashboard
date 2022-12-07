7ceecc538f315a32882f41788a726a8afdff0d88
Novalia9517 <novalia.9517@gmail.com>
2022-12-07 13:38:17 +0700

    Perbaikan search-bar

--- src/Pages/UsersPage.jsx
+++ src/Pages/UsersPage.jsx
@@ -5,7 +5,7 @@ import TableList from '../Components/TableList';
 const UsersPage = () => {
     return (
         <div className='p-10'>
-            <SearchBar />
+            <SearchBar title={'Users List'} description={'Create, Edit Or Delete Users'}/>
             <div className="card border border-border-primary">
                 <div className="card-body">
                     <TableList />
